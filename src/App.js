38c433a65529ca3a2cec3bdfe3378ec1473a12a8
srikanthdevabattula <122144374+srikanthdevabattula@users.noreply.github.com>
2023-09-27 22:06:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+// npm install bootstrap
+// npm install --save @fortawesome/react-fontawesome @fortawesome/free-solid-svg-icons
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
